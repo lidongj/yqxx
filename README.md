032cf06e846b06732fd85b5450e3a11a68486a9b
billchenchina <billchenchina2001@gmail.com>
2021-08-15 23:46:03 +0800

    nextgen

--- README.md
+++ README.md
@@ -27,30 +27,173 @@ username: '1234567890'
 # 统一身份认证密码
 password: 'PASSWORD'
 
-# 体温
-# **目前已不需要此项**
-# brzgtw: '36.5'
+# 经度
+gpsjd: 126.630644
+# 纬度
+gpswd: 45.746883
 
-# 国（境）内详细地址
-# 格式为 AAA省BBB市CCC区DD街eeee号
-gnxxdz: '黑龙江省哈尔滨市南岗区工建街88号哈工大招待所'
+# 居住地址（Residence Address）（请详细填写至门牌号，如：X省X市X区X街X小区/楼宇X单元X门牌号）(Road and doorplate，e.g.：Room XX No. XX Community XX)
+# 走读需要填写
+jzdz: ''
 
-# 当前状态
-# **目前已不需要此项**
-# 1 离校
-# 01 在校（校内宿舍住）
-# 03 居家
-# 04 探亲
-# 05 访友
-# 06 旅行
-# 07 会议
-# 99 其他
-# dqztm: '03'
+# 所在地点（Currrent location）
+# 0 国（境）外（Overseas）
+# 1 国内（Domestic）
+kzl1: 1
 
-# 当前所在区
-# **目前已不需要此项**
-# 为当前所在区的行政区划代码（即身份证号前六位）
-# dqszdqu: '230103'
+# 所在国家或地区（Current Country or region Residing in）
+# 国（境）外（Overseas）需要填写
+kzl2: ''
+
+# 所在国家城市（Current City Residing in）
+# 国（境）外（Overseas）需要填写
+kzl3: ''
+
+# 所在国家具体地址（Address Residing in）
+# 国（境）外（Overseas）需要填写
+kzl4: ''
+
+# 近1个月是否计划回国（Do you plan to return to China within one month）
+# 国（境）外（Overseas）需要填写
+# 0 否
+# 1 是
+kzl5: ''
+
+# 定位信息建议通过 https://lbs.amap.com/demo/javascript-api/example/geocoder/regeocoding F12 获取
+# 定位省
+kzl6: '黑龙江省'
+
+# 定位市
+kzl7: '哈尔滨市'
+
+# 定位区
+kzl8: '南岗区'
+
+# 定位详细地址
+# 街道名+门牌号
+kzl9: '教化街30号'
+
+# 定位信息
+# 地图定位地点
+kzl10: '黑龙江省哈尔滨市南岗区花园街道教化街哈尔滨工业大学'
+
+# 与上次定位在不同城市的原因
+# 0 探亲（Visiting relatives）
+# 1 旅游（Traveling）
+# 2 回家（Homecoming）
+# 3 因公出差/实习实训（Business /practical reasons）
+# 4 其他（Others）
+kzl11: ''
+
+# 与上次定位在不同城市的原因
+kzl12: ''
+
+# 您当前所在地点是（Your current location is）(单元/社区/街道等) (unit/community/street, etc.)
+# 0 低风险地区（Low risk area）
+# 1 中风险地区（Medium risk area）
+# 2 高风险地区（High risk areas）
+kzl13: 0
+
+# 您所处中/高风险地区所在街道，社区名称（The street and community name of your medium/high risk area is）
+# 例：哈尔滨市呼兰区兰河街道沿河社区
+kzl14: ''
+
+# 当日是否途径中高风险地区（Any contact with medium or high risk area?）
+# 0 否
+# 1 是
+kzl15: 0
+
+# 中/高风险地区所在街道，社区名称（The street and community name of medium/high risk area is）
+# 例：哈尔滨市呼兰区兰河街道沿河社区
+kzl16: ''
+
+# 今日体温范围（Today's temperature）
+# 0 37.2℃及以上（Greater then or equal to 37.2°C；）
+# 1 37.2°C以下（ below 37.2°C）
+kzl17: 1
+
+# 今日是否出现不适（多选）？（Do you have any of the following symptoms（Multiple choice）?）
+# 0 无不适（Asymptomatic）
+# 1 乏力（Fatigue）
+# 2 干咳（Dry cough）
+# 3 呼吸困难（Difficulty in breathing）
+# 4 其他（Other symptoms）
+kzl18: 0
+
+# 是否到相关医院或门诊检查（Did you go to a hospital or clinic for a check-up?)
+# 0 否
+# 1 是
+kzl19: ''
+
+# 检查结果（Examination results)
+# 0 疑似感染（Suspected）
+# 1 确诊感染（Infected）
+# 2 其他（Others）
+kzl20: ''
+
+# 其他（Others）
+kzl34: ''
+
+# 自行采取的救护措施？（Has any medications been taken by oneself?）
+# 0 已口服药物，无其他异常(Already took oarl medication, no other symptoms)
+# 1 未服药物，无其他异常（Didn't take any medications, no other symptoms)
+# 2 其他情况（Other situations）
+kzl21: ''
+
+# 其他情况（Other situations）
+kzl22: ''
+
+# 当前的健康状况（Current health status）
+# 0 正常（Normal）
+# 1 新冠肺炎无症状感染者（Novel coronavirus pneumonia asymptomatic infection）
+# 2 新冠肺炎确诊病例（Confirmed cases of novel coronavirus pneumonia）
+kzl23: 0
+
+# 是否处于隔离期？（Are you currently in an isolation period?）
+# 0 否
+# 1 是
+kzl24: 0
+
+# 隔离场所（Isolation place）
+# 0 定点医院（Designated hospital）
+# 1 集中隔离点（Centralized isolation location）
+# 2 居家隔离（Isolate at home）
+kzl25: ''
+
+# 隔离详细地址（The detailed address of isolation）
+# （请填写隔离医院/集中隔离点/居家隔离详细地址，精确到门牌号）(please fill in the detailed address of the isolated hospital/centralized isolation location/isolated at home, accurate to the door number)
+kzl26: ''
+
+# 隔离开始时间（Start date of isolation）
+# 示例：2021-8-15
+kzl27: ''
+
+# 本人或共同居住的家人是否与确诊病例、无症状感染者、疑似病例行程轨迹有交集？（Do you or your family members living together cross paths with confirmed /asymptomatic/ suspected cases?）
+# 0 否（No）
+# 1 是（Yes）
+kzl28: 0
+
+# 是否与确诊病例、无症状感染者乘坐同次航班和列车（Has a confirmed or asymptomatic case  been detected in same transportation with you）
+# 0 否（No）
+# 1 是（Yes）
+kzl29: 0
+
+# 请详细说明（Please specify）
+kzl30: ''
+
+# 本人48小时内是否已进行核酸检测（Has COVID-19 nucleic acid test been conducted during 48 hours）
+# 0 否（No）
+# 1 是（Yes）
+kzl31: ''
+
+# 目前本人新冠疫苗接种情况（Vaccination status）
+# 0 未接种(Unvaccinated)
+# 1 已接种部分剂次(Single dose has been inoculated)
+# 2 已接种全部剂次(Both doses inoculated)
+kzl32: 0
+
+# 其他信息（Other information）
+kzl33: '无'
 ```
 
 ## 开源许可证
--- yqxx/yqxx.py
+++ yqxx/yqxx.py
@@ -48,10 +48,11 @@ def read_config(filename: str) -> Tuple[str, str, str]:
         logger.info("Reading config from %s", filename)
         o = open(filename, 'r', encoding='utf-8')
         c = yaml.load(o, Loader=yaml.SafeLoader)
-        for k in c:
-            c[k] = str(c[k])
-        ret = (c['username'], c['password'], c['gnxxdz'])
-        logger.debug(ret)
+        ret = {}
+        restricted_keys = ["gpsjd","gpswd","jzdz","kzl1","kzl2","kzl3","kzl4","kzl5","kzl6","kzl7","kzl8","kzl9","kzl10","kzl11","kzl12","kzl13","kzl14","kzl15","kzl16","kzl17","kzl18","kzl19","kzl20","kzl21","kzl22","kzl23","kzl24","kzl25","kzl26","kzl27","kzl28","kzl29","kzl30","kzl31","kzl32","kzl33","kzl34"]
+        for k in restricted_keys:
+            ret[k] = str(c[k])
+        ret = (c['username'], c['password'], ret)
         return ret
     except OSError:
         logger.error('Fail to read configuration from %s', filename)
@@ -77,7 +78,7 @@ def main():
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
-    (username, password, gnxxdz) = read_config(args.conf_file)
+    (username, password, data) = read_config(args.conf_file)
     logger.info('Logging in to xg.hit.edu.cn')
     try:
         s = idslogin(username, password)
@@ -101,79 +102,14 @@ def main():
         logger.error('Login failed')
         sys.exit(1)
     logger.info('Login success')
-    r = s.post('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsb/csh')
-    _ = json.loads(r.text)
-    if _['isSuccess']:
-        module = _['module']
-        logger.info("Successfully created yqxx")
-        logger.debug("yqxx id: %s", module)
-    else:
-        module = ''
-        r = s.post('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xs/getYqxxList')
-        yqxxlist = json.loads(r.text)
-        logger.debug(yqxxlist)
-        if not yqxxlist['isSuccess']:
-            logger.error(
-                'Fail to getYqxxList, msg: %s', yqxxlist['msg'])
-            sys.exit(1)
-        yqxxlist = yqxxlist['module']['data']
-        for i in yqxxlist:
-            if i['rq'] == date.today().isoformat():
-                if i['zt'] == '00':  # 未提交
-                    module = i['id']
-                    logger.info("Using previously created yqxx")
-                    logger.debug("yqxx id: %s", module)
-                elif i['zt'] == '01':  # 待辅导员审核
-                    if args.force:
-                        logger.info("You have already submitted yqxx")
-                        module = i['id']
-                        logger.info("Using previously created yqxx")
-                        logger.debug("yqxx id: %s", module)
-                    else:
-                        logger.error("Find previously created yqxx, EXITING!")
-                        sys.exit(0)
-                else:  # 辅导员审核成功 当前状态不可提交！
-                    logger.info("You cannot submit at this time!")
-                    sys.exit(0)
-                break
-    if not module:
-        logger.error('Could not find yqxx!')
-        sys.exit(1)
     data = {
         'info': json.dumps({
-            "model": {
-                "id": module,
-                "brfsgktt": "0",  # 是否有发烧、干咳、头痛等症状？
-                "brsfgl": "0",  # 本人是否处于隔离期？
-                "brsfjy": "0",  # 是否就医
-                "brsflt": "0",  # 本人是否被社区、疾控中心等部门流调？
-                "brsfmqjc": "0",  # 本人是否为密切接触者或二次密切接触者？
-                "brsfqzbl": "0",  # 本人是否确诊病例？
-                "brsfwzzbl": "0",  # 本人是否无症状病例？
-                "brzdjlbz": "",  # 医院诊断结果
-                "dqszd": "01",  # 01 国（境）内, 02 海外
-                "gllx": "",  # 隔离类型
-                "glyy": "",  # 隔离原因
-                "glyybz": "",  # 其他原因
-                "gnxxdz": gnxxdz,  # 详细地址（定位）
-                "hsjcjg": "",  # 检测结果
-                "hwcs": "",  # 海外城市
-                "hwgj": "",  # 海外国家
-                "hwxxdz": "",  # 海外详细地址
-                "jrtw": "0",  # 今日体温
-                "jtszdsfzgfx": "0",  # 家庭所在地是否为中高风险地区？
-                "qtbgsx": "",  # 其他需要报告的事项
-                "sfhsjc": "0",  # 是否核酸检测？
-                "sftjwhjhb": "0",  # 14天内是否途径中高风险地区或境外返回？
-                "sftzrychbwhhl": "0",  # 本人或共同居住人是否与确诊、疑似、无症状病例行程轨迹有交集？
-                "sjszdsfzgfx": "0",  # 实际所在地是否为中高风险地区？
-                "sjxxdz": "",  # 详细居住地
-            }
+            "model": data
         })
     }
     logger.debug("data: %s", data['info'])
     r = s.post(
-        'https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsb/saveYqxx', data=data)
+        'https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew/save', data=data)
     logger.debug(r.text)
     j = json.loads(r.text)
     logger.debug(j)
