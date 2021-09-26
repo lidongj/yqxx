5be74df548b56b147041819c5715467077b2dd1d
lidongj <89124900+lidongj@users.noreply.github.com>
2021-09-26 18:08:23 +0800

    Add warning depending on time

--- yqxx/yqxx.py
+++ yqxx/yqxx.py
@@ -4,7 +4,7 @@ import logging
 import random
 import sys
 import urllib
-from datetime import date
+from datetime import datetime, timedelta, timezone
 from typing import Tuple
 
 import yaml
@@ -94,8 +94,8 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
     if not args.yes_i_know_yqxx_enough_go_ahead_please:
-        logging.error('''\
-Yqxx could be risky if you don't understand how it works.\
+        logging.error('''
+Yqxx could be risky if you don't understand how it works.
 If you still confirm to use yqxx, please re-run yqxx with:
     --yes-i-know-yqxx-enough-go-ahead-please
 ''')
@@ -153,6 +153,12 @@ If you still confirm to use yqxx, please re-run yqxx with:
         sys.exit(1)
     if Znx != 0:
         logger.error('您有未阅读的消息，请尽快阅读。')
+        sys.exit(1)
+    if datetime.now(timezone(timedelta(hours=8))) < datetime.fromisoformat('2021-09-30T20:00:00+08:00'):
+        logging.warning('''
+Yqxx could be risky if you don't understand how it works.
+This notification will stop displaying after Sep 30, 2021.
+''')
         sys.exit(1)
     return
 
